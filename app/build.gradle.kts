e10e06178926e63656f064f0240e17f42faa4ec1
royalcyber7r <118531476+royalcyber7r@users.noreply.github.com>
2026-09-03 04:45:31 +0600

    Add RecyclerView dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,4 +35,5 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.5.1")
     implementation("androidx.media3:media3-ui:1.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
