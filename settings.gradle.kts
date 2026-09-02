e2c248bb42369cbdd5519ef70a51f3c547fce6c4
royalcyber7r <118531476+royalcyber7r@users.noreply.github.com>
2026-09-03 05:50:25 +0600

    Add repositories to plugin and dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
     repositories {
         google()
@@ -8,6 +10,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
