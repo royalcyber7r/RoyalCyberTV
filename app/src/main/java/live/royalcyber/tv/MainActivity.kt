9add685259e9cd76b10fd0d0b499538f403470b1
royalcyber7r <118531476+royalcyber7r@users.noreply.github.com>
2026-09-03 21:41:50 +0600

    Fix formatting in MainActivity.kt for Channel list

--- app/src/main/java/live/royalcyber/tv/MainActivity.kt
+++ app/src/main/java/live/royalcyber/tv/MainActivity.kt
@@ -115,7 +115,7 @@ class MainActivity : AppCompatActivity() {
             name = "ATN Music",
             logo = "https://yt3.googleusercontent.com/fl29COpVoSRu4iXG505zsyWag0d9kdB-I-C2mm8h7ePDiT2SSTqGWyt93ObOHWcLdh0wa7yd=s900-c-k-c0x00ffffff-no-rj",
             streamUrl = "https://app.ncare.live/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/atnmusic.stream/playlist.m3u8"
-
+        ),
         Channel(
             name = "Channel S",
             logo = "https://upload.wikimedia.org/wikipedia/en/a/aa/Channel_S_Bangladesh_Logo.png",
