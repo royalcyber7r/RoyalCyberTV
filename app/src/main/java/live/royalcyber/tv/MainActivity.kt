c455ccc0f76cb1ba67f460265831606c7df5b44d
royalcyber7r <118531476+royalcyber7r@users.noreply.github.com>
2026-09-06 19:23:12 +0600

    Update Zoom channel logo to use WebP format

--- app/src/main/java/live/royalcyber/tv/MainActivity.kt
+++ app/src/main/java/live/royalcyber/tv/MainActivity.kt
@@ -232,7 +232,7 @@ Channel( name = "Zee Bangla",
        ), 
 
 Channel( name = "Zoom",
-        logo = "https://raw.githubusercontent.com/royalcyber7r/RoyalCyberTV/logo/app/src/main/logos/zoom-tv.png",
+        logo = "https://raw.githubusercontent.com/royalcyber7r/RoyalCyberTV/logo/app/src/main/logos/zoom.webp",
         streamUrl = "https://dai.google.com/linear/hls/event/JCAm25qkRXiKcK1AJMlvKQ/master.m3u8" 
        ), 
 
